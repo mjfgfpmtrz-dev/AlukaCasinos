74af162845720c9a9fcf7c7fa4260a65e53994ff
Necropof <mjfgfpmtrz@privaterelay.appleid.com>
2025-12-19 22:27:39 +0100

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-function show(id) {
-  document.querySelectorAll('.screen').forEach(s => s.classList.remove('active'));
-  document.getElementById(id).classList.add('active');
-
-  document.querySelectorAll('.nav button').forEach(b => b.classList.remove('active'));
-}
